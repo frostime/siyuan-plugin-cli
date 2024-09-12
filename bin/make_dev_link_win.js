a8a172d109a4a6c5feffbf56f15c0115302dcbf6
frostime <frostime@foxmail.com>
2024-09-12 11:20:11 +0800

    🐛 fix: 修复 make-link-win 工作目录不正确的问题

--- bin/make_dev_link_win.js
+++ bin/make_dev_link_win.js
@@ -1,12 +1,29 @@
 #!/usr/bin/env node
 
 import { exec } from 'child_process';
-// import path from 'node:path';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-exec(`powershell -File ./bin/make_dev_link_win.ps1`, (error, stdout, stderr) => {
+const scriptPath = path.join(__dirname, 'make_dev_link.js');
+const workingDirectory = process.cwd();
+// console.log(`Working directory: ${workingDirectory}`);
+
+// Use Set-Location to change the working directory in PowerShell
+const run = `Set-Location -Path "${workingDirectory}"; node "${scriptPath}"; pause`;
+
+const command = `
+powershell -Command "Start-Process -Verb RunAs powershell.exe -Args '-Command \"${run}\"'"
+`.trim();
+
+// console.log(`Running command: ${command}`);
+
+exec(command, { cwd: workingDirectory }, (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
+        console.error(stderr);
         return;
     }
     console.log(stdout);
--- bin/make_dev_link_win.ps1
+++
@@ -1,6 +0,0 @@
-$scriptDir = $PSScriptRoot
-
-$elevateScript = Join-Path $scriptDir "elevate.ps1"
-$makeDevLinkScript = Join-Path $scriptDir "make_dev_link.js"
-
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File $elevateScript -scriptPath $makeDevLinkScript
